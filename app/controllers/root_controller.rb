dc6078baa10164d03d637ee3154af23771d3e905
mar bautista <marilirulita@gmail.com>
2022-12-07 23:19:28 -0800

    install rails with react webpack

---
+++ app/controllers/root_controller.rb
@@ -0,0 +1,5 @@
+class RootController < ApplicationController
+  def index
+    @users = [1, 2, 3, 4]
+  end
+end
---
+++ app/javascript/application.js
@@ -0,0 +1,23 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Test project rails react</h1>
+      <form action="/users" method="post">
+        <input type="text" name="user[name]" />
+        <input type="submit" value="Create User" />
+      </form>
+      <div></div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
