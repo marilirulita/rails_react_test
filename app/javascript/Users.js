1f8c99668f6cbaf654b9c60704b3c1adbb93ae0c
mar bautista <marilirulita@gmail.com>
2022-12-30 16:30:34 -0800

    add useref in user to manage name input

--- app/javascript/Users.js
+++ app/javascript/Users.js
@@ -1,23 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from './redux/usersReducer';
 
 const Users = () => {
   const dispatch = useDispatch();
   const users = useSelector((store) => store.users);
+  const userNameRef = useRef();
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(userNameRef.current.value)
+    // const userName = userNameRef.current.value;
+    // const formData = new FormData();
+    // formData.append('user[name]', userName);
+    // fetch('/users', {
+    //   method: 'POST',
+    //   body: formData,
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     dispatch(fetchUsers());
+    //   });
+  };
+
+
   return (
     <div>
-      <h1>Users</h1>
-      <form action="/users" method="post">
-        <input type="text" name="user[name]" />
+      <h1>{users.users}</h1>
+      <form onSubmit={onSubmit} action="/users" method="post">
+        <input ref={ userNameRef } type="text" name="user[name]" />
         <input type="submit" value="Create User" />
       </form>
-      <div> {users.users} </div>
     </div>
   );
 }
