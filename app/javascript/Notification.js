3dd612006622d06d0f0fd479c538ca72e89b9676
mar bautista <marilirulita@gmail.com>
2023-01-04 02:51:07 -0800

    add close function in alert

--- app/javascript/Notification.js
+++ app/javascript/Notification.js
@@ -1,12 +1,16 @@
-import React from "react";
-import { Alert } from "@mui/material";
+import React, { useState } from "react";
+import { Alert, Collapse, IconButton, CloseIcon } from "@mui/material";
 import { useSelector } from "react-redux";
 
 const Notification = ({type, mess}) => {
+  const [open, setOpen] = useState(true);
   const notification = useSelector((state) => state.alert.alert);
   return (
     <div>
-      { notification && <Alert severity={type}>{mess}</Alert> }
+      { notification && 
+      <Collapse in={open}>
+        <Alert onClose={() => {setOpen(false)}} severity={type}>{mess}</Alert>
+        </Collapse> }
     </div>
   );
 }
